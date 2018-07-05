66014e5e6fcc2bd6defd2e99bb23d7a89d412369
Shroompow <go-lion@gmx.de>
2018-07-05 22:35:49 +0200

    Changed Module Event Handling

--- constants.js
+++ constants.js
@@ -1,3 +1,8 @@
 module.exports = {
 	//key: value
+	/**
+	 * On receiving a command.
+	 * @argument {CommandData} cmd
+	 */
+	MODULE_EVENT_CMD = "cmd"
 }
--- modules/module.js
+++ modules/module.js
@@ -1,61 +1,16 @@
+const Events = require('events');
 /**
  * @description ModuleBaseClass
  */
-class Module {
+class Module extends Events {
 	/**
 	 * @param {State} state The Bot-State
 	 * @param {string} name Name of the module
 	 */
 	constructor(state, name) {
 		this.state = state;
+		this.name = name;
 	}
-	/**
-	 * @description Initialize Module.
-	 */
-	async init() {
-
-	}
-	/**
-	 * @description Called after global initializing finished.
-	 */
-	postInit() {
-
-	}
-	/**
-	 * @description Called before bot exits.
-	 */
-	clearUp() {
-
-	}
-	/**
-	 * Called when Module gets enabled at a Location.
-	 * @param {Location} location The Location where the module has been enabled.
-	 */
-	onEnable(location) {
-
-	}
-	/**
-	 * Called when Module gets disabled at a Location.
-	 * @param {Location} location The Location where the Module has been disabled.
-	 */
-	onDisable(location) {
-
-	}
-	/**
-	 * Called on receiving a Message.
-	 * @param {Message} msg 
-	 */
-	onMsg(msg) {
-
-	}
-	/**
-	 * Called on receiving a Command.
-	 * @param {CommandData} cmd The Command received.
-	 */
-	onCmd(cmd) {
-
-	}
-
 }
 
 module.exports = Module;
