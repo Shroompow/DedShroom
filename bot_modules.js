ed28d3fbe77e374f102ab0b389a0c61034b08487
Shroompow <go-lion@gmx.de>
2018-07-05 20:30:30 +0200

    Pre-Orginazation

---
+++ bot_modules.js
@@ -0,0 +1,10 @@
+module.exports = [
+    /*
+    {name:"",displayName:""}
+    name:
+        Name of the module-file.
+    displayName:
+        Displayed Name of the module.
+        If not set, module will be invisible.
+    */
+]
