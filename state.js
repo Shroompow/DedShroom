9cc9fd9a862b47429567db8ff5203837e1b6c0a0
Shroompow <go-lion@gmx.de>
2018-07-07 20:19:57 +0200

    State IndexLoading

--- state.js
+++ state.js
@@ -1,16 +1,25 @@
 const discord = require('discord.js');
+const constants = require('./constants.js');
 class State {
 	constructor(){
 		this.modules = {};
 		this.guilds = {};
 		this.users = {};
 		this.sessions = {};
+		this.indexes = {};
 		this.running = false;
 	}
 	async start(token){
 		if(running)return;
 
-		//TODO: Load/Initialize/MumboJumbo
+		//Loading indexes
+		try{
+			var languagesIndex = this.indexes["languages"] = require(constants.FILE_LANGUAGES);
+			var modulesIndex = this.indexes["modules"] = require(constants.FILE_MODULES);
+		}catch(e){
+			console.error("Error on loading Indexes",e.message);
+			process.exit();
+		}
 		this.running = true;
 	}
 	async end(force){
