843fa4bba7514e7cdc6441a7eb3fbbbc91bf6aee
Shroompow <go-lion@gmx.de>
2018-07-07 17:16:58 +0200

    Added constants for directories and files

--- constants.js
+++ constants.js
@@ -4,5 +4,10 @@ module.exports = {
 	 * On receiving a command.
 	 * @argument {CommandData} cmd
 	 */
-	MODULE_EVENT_CMD = "cmd"
+	MODULE_EVENT_CMD = "cmd",
+	DIR_MODULES = "./modules/",
+	DIR_LANG = "./lang/",
+	FILE_COMMAND = "./utils/command.js",
+	FILE_MODULES = "./modules/modules.js",
+	FILE_LANGUAGES = "./lang/languages.js"
 }
