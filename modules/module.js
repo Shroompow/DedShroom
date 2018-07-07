e062226063a7d577231d2e7d570066c13a1f5fcd
Shroompow <go-lion@gmx.de>
2018-07-08 00:28:24 +0200

    Updated Module BaseClass

--- modules/module.js
+++ modules/module.js
@@ -4,13 +4,33 @@ const Events = require('events');
  */
 class Module extends Events {
 	/**
-	 * @param {State} state The Bot-State
 	 * @param {string} name Name of the module
+	 * @param {string} displayName DisplayName of the module
+	 * @param {boolean} nsfw Is the module nsfw?
 	 */
-	constructor(state, name,nsfw=false) {
-		this.state = state;
+	constructor(name, displayName="",nsfw=false) {
+		/**Name of the module */
 		this.name = name;
+		/**Displayed name of the module */
+		this.displayName = displayName;
+		/**Is the bot nsfw? */
 		this.nsfw = nsfw;
+	}
+	/**
+	 * Initialize the Module
+	 * @param {State} state The bot-state
+	 */
+	init(state){
+		/**The state the module belongs too */
+		this.state = state;
+	}
+	/**Post Initialize Module */
+	postInit(){
+
+	}
+	/**Exit the Module */
+	exit(){
+
 	}
 }
 
