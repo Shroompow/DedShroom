e3a086de8a91a856eafda4719d226bf52e16b700
Shroompow <go-lion@gmx.de>
2018-07-09 20:17:27 +0200

    Logger Clean up

--- dedshroom/utils/logging.js
+++ dedshroom/utils/logging.js
@@ -2,123 +2,200 @@ const formatter = require('./format.js');
 const consts = require('../constants.js');
 
 LEVEL = {
-  DEBUG: 1,
-  INFO: 2,
-  WARNING: 3,
-  CRITICAL: 4
+	DEBUG: 1,
+	INFO: 2,
+	WARNING: 3,
+	CRITICAL: 4,
+	MAX: 4
 }
 
 class LogManager {
-  constructor(format=consts.LOGGER_FORMAT, level=LEVEL.INFO) {
-    this.cache = [];
-    this.format = format;
-    this.level = level;
-  }
+	/**
+	 * Constructs a LogManager.
+	 * @param {string} [format] String to be used to format messages.
+	 * @param {number} [level] Minimum Level required to show messages.
+	 */
+	constructor(format = consts.LOGGER_FORMAT, level = LEVEL.INFO) {
+		this.cache = [];
+		this.format = format;
+		this.level = Math.min(level, LEVEL.MAX);
+	}
 
-  // get the (cached) logger for the namespace
-  get(namespace) {
-    var res = this.cache.find(logger => logger.namespace == namespace);
+	/**
+	 * Gets the Logger from a namespace.
+	 * If the namespace hasn't been createdy yet, a new Logger will be created.
+	 * @param {string} namespace The namespace.
+	 * @returns {Logger} The created or cached Logger.
+	 */
+	get(namespace) {
+		var res = this.cache.find(logger => logger.namespace == namespace);
 
-    // initialize logger if none exists
-    if (res === undefined) {
-      res = new Logger(namespace, this);
-      this.cache.push(res);
-    }
+		// initialize logger if none exists
+		if (res === undefined) {
+			res = new Logger(namespace, this);
+			this.cache.push(res);
+		}
 
-    return res;
-  }
+		return res;
+	}
 
-  remove(namespace) {
-    this.cache = this.cache.filter(logger => logger.namespace != namespace);
-  }
+	/**
+	 * Removes a namespace.
+	 * @param {string} namespace The namespace to remove
+	 */
+	remove(namespace) {
+		this.cache = this.cache.filter(logger => logger.namespace != namespace);
+	}
 
-  // include the loglevel enum
-  get LEVEL() {
-    return LEVEL;
-  }
+	/**
+	 * @returns {object} Object containing Log-Levels
+	 */
+	get LEVEL() {
+		return LEVEL;
+	}
 }
 
-// build an env to use for formatter.format()
-function makeEnv(namespace, level, message='') {
-    var levelString;
-    switch (level) {
-      case LEVEL.DEBUG:
-        levelString = 'DEBUG';
-        break;
-      case LEVEL.WARNING:
-        levelString = 'WARNING';
-        break;
-      case LEVEL.CRITICAL:
-        levelString = 'CRITICAL';
-        break;
-      default:
-        levelString = 'INFO';
-    }
+/**
+ * build an env to use for formatter.format()
+ * @param {string} namespace 
+ * @param {number} level 
+ * @param {string} message 
+ * @private
+ */
+function makeEnv(namespace, level, message = '') {
+	var levelString;
+	switch (level) {
+		case LEVEL.DEBUG:
+			levelString = 'DEBUG';
+			break;
+		case LEVEL.WARNING:
+			levelString = 'WARNING';
+			break;
+		case LEVEL.CRITICAL:
+			levelString = 'CRITICAL';
+			break;
+		default:
+			levelString = 'INFO';
+	}
 
-    return {
-      namespace: namespace,
-      date: Date.now(),
-      level: levelString,
-      message: message,
-    };
-  }
+	return {
+		namespace: namespace,
+		date: Date.now(),
+		level: levelString,
+		message: message,
+	};
+}
 
 class Logger {
-  constructor(namespace, manager) {
-    this.namespace = namespace;
-    this.manager = manager;
-    this._level = undefined;
-    this.silenced = false;
-  }
+	/**
+	 * Constructs a new Logger.
+	 * DONT USE THIS!
+	 * Use LogManager.get() instead.
+	 * @param {string} namespace Namespace the Logger belongs to.
+	 * @param {LogManager} manager LogManager the Logger belongs to.
+	 */
+	constructor(namespace, manager) {
+		this.namespace = namespace;
+		this.manager = manager;
+		this._level = undefined;
+		this.silenced = false;
+	}
 
-  get level() {
-    return this._level !== undefined ? this._level : this.manager.level;
-  }
+	/**
+	 * @returns {number} The Loggers Minimum Level.
+	 */
+	get level() {
+		return this._level !== undefined ? this._level : this.manager.level;
+	}
 
-  set level(value) {
-    this._level = value;
-  }
+	/**
+	 * Sets the Loggers Minimum Level.
+	 */
+	set level(value) {
+		this._level = value;
+	}
 
-  resetLevel() {
-    this.level = undefined;
-  }
+	/**
+	 * Resets the Loggers Minimum Level.
+	 */
+	resetLevel() {
+		this.level = undefined;
+	}
 
-  silence() {
-    this.silenced = true;
-  }
+	/**
+	 * Silences the Logger.
+	 */
+	silence() {
+		this.silenced = true;
+	}
 
-  unsilence() {
-    this.silenced = false;
-  }
+	/**
+	 * Unsilences the Logger.
+	 */
+	unsilence() {
+		this.silenced = false;
+	}
 
-  log(level, msg='') {
-    // don't log if silenced
-    if (!this.silenced) {
-      // check if the log level is enabled
-      if (level >= this.level) {
-        var env = makeEnv(this.namespace, level, msg.toString())
-        var logMessage = formatter.format(this.manager.format, env)
-        console.log(logMessage)
-      }
-    }
-  }
+	/**
+	 * Is silenced?
+	 * @returns {boolean}
+	 */
+	isSilenced(){
+		return this.silenced;
+	}
 
-  // shorthands
-  debug(msg='') {
-    this.log(LEVEL.DEBUG, msg);
-  }
+	/**
+	 * Logs a Message at specified Level.
+	 * @param {number} level Level to log at. See
+	 * @param {string} msg 
+	 */
+	log(level, msg = '') {
+		// don't log if silenced
+		if (!this.silenced) {
+			// check if the log level is enabled
+			if (level >= this.level) {
+				var env = makeEnv(this.namespace, level, msg.toString());
+				var logMessage = formatter.format(this.manager.format, env);
+				if(level<LEVEL.WARNING)
+					console.log(logMessage);
+				else
+					console.error(logMessage);
+				
+			}
+		}
+	}
 
-  info(msg='') {
-    this.log(LEVEL.INFO, msg);
-  }
+	/**
+	 * Shorthand for logging at DEBUG-Level
+	 * @param {string} msg 
+	 */
+	debug(msg = '') {
+		this.log(LEVEL.DEBUG, msg);
+	}
 
-  warning(msg='') {
-    this.log(LEVEL.WARNING, msg);
-  }
+	/**
+	 * Shorthand for logging at INFO-Level
+	 * @param {string} msg 
+	 */
+	info(msg = '') {
+		this.log(LEVEL.INFO, msg);
+	}
 
-  critical(msg='') {
-    this.log(LEVEL.CRITICAL, msg);
-  }
+	/**
+	 * Shorthand for logging at WARNING-Level
+	 * @param {string} msg 
+	 */
+	warning(msg = '') {
+		this.log(LEVEL.WARNING, msg);
+	}
+
+	/**
+	 * Shorthand for logging at CRITICAL-Level
+	 * @param {string} msg 
+	 */
+	critical(msg = '') {
+		this.log(LEVEL.CRITICAL, msg);
+	}
 }
 
 module.exports = new LogManager();
