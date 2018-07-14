3bb6c6a2176b075e4a7f01e3d0dec282c7651065
Shroompow <go-lion@gmx.de>
2018-07-14 15:20:43 +0200

    Added Command-Class and Updated CommandData

--- dedshroom/utils/command.js
+++ dedshroom/utils/command.js
@@ -1,31 +1,49 @@
+const format = require("./format.js");
+const eventOrigin = require("./eventOrigin.js");
+
 /**
- * @description Command class
+ * @description CommandData class
  */
 class CommandData {
 	/**
-	 * 
-	 * @param {string} name Name of the command
+	 * @param {Command} command The command
 	 * @param {string[]} args Array of arguments
-	 * @param {Location} location Location where command has been emitted
-	 * @param {User} emitter Emitter of the command
+	 * @param {EventOrign} origin EventOrigin where command has been emitted
 	 * @param {Message} msg Original message
+	 * @param {User} [emitter] Emitter of the command
 	 */
-	constructor(name,args,location,emitter,msg){
-		Object.assign(this,{
-			name:name,
-			args:args,
-			location:location,
-			emitter:emitter,
-			msg:msg
-		});
+	constructor(command, args, origin, msg) {
+		this.command = command;
+		this.args = args;
+		this.origin = origin;
+		this.msg = msg;
 	}
 	/**
-	 * 
+	 * Parses a CommanData-Object from a string.
 	 * @param {string} str String to parse
+	 * @param {EventOrigin} origin Origin of command
+	 * @returns {EventOrigin|undefined} A new instance of CommandData or undefined if the command could not be parsed.
 	 */
-	static parse(str){
-
+	static parse(str,origin) {
+		var [name,...args] = format.argSplit(str);
+		if(name){
+			var cmd;
+			//TODO: Find command-object
+			if(cmd){
+				return new CommandData(cmd,args,origin,str);
+			}
+		}
+		return undefined;
 	}
 }
 
-module.exports = CommandData;
+class Command {
+	constructor(owner, name, nsfw) {
+		this.owner = owner;
+		this.name = name;
+		this.nsfw = nsfw;
+	}
+}
+
+module.exports.CommandData = CommandData;
+module.exports.Command = Command;
